6393ae8d5d867b3d9f2cc7527e53570adfb67f55
Patrick Monkam <aurelpatrickm@gmail.com>
2025-11-06 09:54:50 -0500

    feat: first view

---
+++ NutriLog/Screens/LocalAuthentificationView.swift
@@ -0,0 +1,54 @@
+//
+//  LocalAuthentificationView.swift
+//  NutriLog
+//
+//  Created by Patrick Aurel Monkam Ngaleu on 2025-11-06.
+//
+import SwiftUI
+import LocalAuthentication
+
+struct LocalAuthentificationView: View {
+    
+    
+    @State private var isAuthenticated: Bool = false
+    
+    var body: some View {
+        VStack {
+            if isAuthenticated {
+               HomeView()
+            } else {
+                Text ("Please Authenticate")
+                    Button {
+                    authentificate()
+                        
+                    } label: {
+                        Text("Retry.")
+                    }
+            }
+        }
+        .onAppear(){
+            authentificate()
+        }
+    }
+    
+    func authentificate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "We need to unlock data"
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) {
+                success, error in
+                if let error {
+                    print (error)
+                    return
+                }
+                if success {
+                    print("Authentificated.")
+                }
+                   isAuthenticated = success
+                }
+            }
+        }
+    }
+
--- NutriLog/Screens/LoginView.swift
+++ NutriLog/Screens/LoginView.swift
@@ -2,7 +2,29 @@ import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        Text("Vue Connexion")
+        Color.loginBackground.ignoresSafeArea()
+            .overlay(
+                VStack(spacing: 0) {
+                    Image("nutrilog-logo")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                    
+                    Button{
+                        
+                    } label: {
+                        Image(systemName: "faceid")
+                        Text("Se connecter")
+                        
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .buttonBorderShape(.roundedRectangle(radius: 10))
+                    .tint(.loginButton)
+        
+                }
+                
+            )
+        
+        
     }
 }
 
